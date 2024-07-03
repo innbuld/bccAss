5c5b1267130cb27316908c13d69e5b5aadf438a6
innbuld <kanmiolanrewaju@gmail.com>
2024-07-03 22:16:16 +0100

    boxData

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,13 @@
 import { useState, useEffect } from 'react';
 
 const assets = [
-  { id: 'BTC', name: 'Bitcoin', symbol: 'BTC/USD' },
-  { id: 'ETH', name: 'Ethereum', symbol: 'ETH/USD' },
-  { id: 'DOGE', name: 'Dogecoin', symbol: 'DOGE/USD' },
-  { id: 'ALGO', name: 'Algorand', symbol: 'ALGO/USD' },
-  { id: 'DOT', name: 'Polkadot', symbol: 'DOT/USD' },
-  { id: 'UNI', name: 'Uniswap', symbol: 'UNI/USD' },
-  { id: 'COMP', name: 'Compound', symbol: 'COMP/USD' },
+  { id: 'BTC', name: 'Bitcoin', symbol: 'BTC/USD', imageUrl: './bt.png' },
+  { id: 'ETH', name: 'Ethereum', symbol: 'ETH/USD', imageUrl: '/btc.png' },
+  { id: 'DOGE', name: 'Dogecoin', symbol: 'DOGE/USD', imageUrl: '/btc.png' },
+  { id: 'ALGO', name: 'Algorand', symbol: 'ALGO/USD', imageUrl: '/btc.png' },
+  { id: 'DOT', name: 'Polkadot', symbol: 'DOT/USD', imageUrl: '/btc.png' },
+  { id: 'UNI', name: 'Uniswap', symbol: 'UNI/USD', imageUrl: '/btc.png' },
+  { id: 'COMP', name: 'Compound', symbol: 'COMP/USD', imageUrl: '/btc.png' },
 ];
 
 export default function Home() {
@@ -32,8 +32,8 @@ export default function Home() {
     <div className="">
       <div className="flex items-center justify-center pt-12">
         <div className="text-center">
-          <p className="font-roboto text-white font-normal text-4xl">Easy send and Request</p>
-          <p className="font-roboto text-white font-normal text-4xl">Crypto</p>
+          <p className="font-roboto text-white font-normal text-6xl">Easy send and Request</p>
+          <p className="font-roboto text-white font-normal text-6xl">Crypto</p>
         </div>
       </div>
 
@@ -48,12 +48,12 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="pt-8 flex items-center justify-center">
+      <div className=" pt-20 flex items-center justify-center">
         <div className="relative bg-customBlack bg-opacity-50 rounded-lg overflow-hidden border border-gray-400 w-full max-w-4xl" style={{ maxWidth: '1180px' }}>
-          <div className="container mx-auto p-4 bg-customBlack bg-opacity-75"> {/* Adjusted bg-opacity to 75 */}
+          <div className="container mx-auto p-4 bg-customBlack bg-opacity-75 pt-12"> 
             <div className="overflow-x-auto">
-              <table className="min-w-full bg-customBlack bg-opacity-75 text-white"> {/* Adjusted bg-opacity to 75 */}
-                <thead className="bg-customBlack bg-opacity-75 text-white border-b border-gray-600"> {/* Adjusted bg-opacity to 75 */}
+              <table className="min-w-full bg-customBlack bg-opacity-75 text-white "> 
+                <thead className="bg-customBlack bg-opacity-75 text-white border-b border-gray-600 "> 
                   <tr>
                     <th className="py-2 px-4 text-left">Asset</th>
                     
@@ -63,11 +63,15 @@ export default function Home() {
                     <th className="py-2 px-4 text-left">Trade</th>
                   </tr>
                 </thead>
-                <tbody>
+                <tbody className=' pt-6'>
                   {assets.map((asset) => (
                     <tr key={asset.id} className="bg-customBlack bg-opacity-75">
-                      <td className="py-2 px-4">{asset.symbol}</td>
-                 
+                      <td className="py-2 px-4 flex items-center ">
+                        <div className="overflow-hidden w-12 h-12 mr-2 rounded-md"> 
+                          <img src={asset.imageUrl} alt={asset.symbol} className="w-full h-full object-cover" />
+                        </div>
+                        {asset.symbol}
+                      </td>
                       <td className="py-2 px-4">{prices[asset.id] || 'Loading...'}</td>
                       <td className="py-2 px-4">-</td>
                       <td className="py-2 px-4">-</td>
