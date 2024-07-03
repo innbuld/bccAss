5f450c56a4810af33dfcb3eef744200697683a91
innbuld <kanmiolanrewaju@gmail.com>
2024-07-03 19:43:17 +0100

    bcc

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "../../components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} relative min-h-screen bg-cover bg-center pt`} style={{ backgroundImage: "url('/bg.png')" }}>
+        <div className=" bg-opacity-70 min-h-screen">
+          <Navbar />
+          <div className="container mx-auto px-4 pt-16">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
+
+
+
