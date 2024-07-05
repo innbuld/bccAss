6d362d9b99eafbe7f3b94d08ee9715000629225f
innbuld <kanmiolanrewaju@gmail.com>
2024-07-05 01:23:58 +0100

    done

--- src/app/api/assets/[id]/route.ts
+++ src/app/api/assets/[id]/route.ts
@@ -1,10 +1,7 @@
-
 import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
-
 const prisma = new PrismaClient();
-
 
 export async function GET() {
   try {
