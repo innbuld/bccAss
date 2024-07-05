d75bd717ccc96eb4f2e2fedc571f4ced5e4ea637
innbuld <kanmiolanrewaju@gmail.com>
2024-07-05 01:45:36 +0100

    api

--- src/app/api/assets/[id]/route.ts
+++ src/app/api/assets/[id]/route.ts
@@ -20,15 +20,14 @@ export async function GET() {
   } catch (error) {
     console.error('Error fetching assets:', error);
     return NextResponse.json({ error: 'Failed to fetch assets' }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
   }
 }
 
 export async function POST(req: NextRequest) {
   try {
-    // Extract data from request body
     const { name, symbol, imageUrl, price, hour, thour } = await req.json();
-
-    // Create a new asset in the database
     const newAsset = await prisma.asset.create({
       data: {
         name,
@@ -52,7 +51,8 @@ export async function POST(req: NextRequest) {
     return NextResponse.json(newAsset, { status: 201 });
   } catch (error) {
     console.error('Error creating asset:', error);
-    // Return an error response if something goes wrong
     return NextResponse.json({ error: 'Failed to create asset' }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
   }
 }
